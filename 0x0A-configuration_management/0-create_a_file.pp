403d6b4c504cc4c0f1bda552cd6903370e27cd5c
brianmaleek <maleekmalik@gmail.com>
2023-09-21 21:22:44 +0300

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,14 @@
+# create a ile in /tmp with the following requirements
+# File path is /tmp/school
+# File permission is 0744
+# File owner is www-data
+# File group is www-data
+# File contains I love Puppet
+
+file { 'school':
+        path    => '/tmp/school',
+        mode    => '0744',
+        owner   => 'www-data',
+        group   => 'www-data',
+        content => 'I love Puppet';
+}
