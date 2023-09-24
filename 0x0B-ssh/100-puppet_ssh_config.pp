01ccd2724d84ba566940deb195b5b13165b9b682
brianmaleek <maleekmalik@gmail.com>
2023-09-24 09:44:04 +0300

    checker configs

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,13 +5,13 @@
 # Your SSH client configuration must be configured to refuse to authenticate using a password
 
 # Setup SSH config using Puppet
-file { 'Turn off password auth':
+file_line { 'Turn off password auth':
   ensure => 'present',
   path   => 'etc/ssh/ssh_config',
   line   => '    passwordAuthentication no',
 }
 
-file { 'Declare identity file':
+file_line { 'Declare identity file':
   ensure => 'present',
   path   => 'etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school',
