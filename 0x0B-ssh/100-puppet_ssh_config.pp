b57df2d975cf16ec15f9d1fe047e9dec34b01e5d
brianmaleek <maleekmalik@gmail.com>
2023-09-24 09:46:56 +0300

    checker fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -7,12 +7,12 @@
 # Setup SSH config using Puppet
 file_line { 'Turn off password auth':
   ensure => 'present',
-  path   => 'etc/ssh/ssh_config',
+  path   => '/etc/ssh/ssh_config',
   line   => '    passwordAuthentication no',
 }
 
 file_line { 'Declare identity file':
   ensure => 'present',
-  path   => 'etc/ssh/ssh_config',
+  path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school',
 }
