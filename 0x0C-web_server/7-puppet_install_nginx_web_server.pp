73f0b7f8d87a600d22b0a1b0df45a7013a593d65
brianmaleek <maleekmalik@gmail.com>
2023-09-28 00:25:40 +0300

    7-checker fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,21 @@
-# Automating nginx server using puppet
+# Automating project requirements using Puppet
 
 package { 'nginx':
-  ensure => 'installed',
+  ensure => installed,
 }
 
 file_line { 'install':
   ensure => 'present',
   path   => '/etc/nginx/sites-enabled/default',
-  after  => 'listen 80 default_server',
+  after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.html':
-  ensure  => 'present',
-  content => 'Hello World',
+  content => 'Hello World!',
 }
 
 service { 'nginx':
-  ensure  => 'running',
+  ensure  => running,
   require => Package['nginx'],
 }
