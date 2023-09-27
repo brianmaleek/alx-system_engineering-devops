48a78de40f6deb65d877ccaf25d27f287e71dc55
brianmaleek <maleekmalik@gmail.com>
2023-09-27 05:45:20 +0300

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,15 +5,15 @@ package { 'nginx':
 }
 
 file { '/var/www/html/index.html':
-  ensure  => 'file',
+  ensure  => present,
   content => 'Hello World',
 }
 
 file_line { 'install':
   ensure => present,
   path   => '/etc/nginx/sites-enabled/default',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com\/watch?v=QH2-TGUlwu4',
   after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com\/watch?v=QH2-TGUlwu4',
 }
 
 service { 'nginx':
