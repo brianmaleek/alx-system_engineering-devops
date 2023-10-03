7c5f3757053c879d956cc448a609023bd3953a77
brianmaleek <maleekmalik@gmail.com>
2023-10-03 05:48:51 +0300

    checker fix 1.0.3

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,15 +1,7 @@
 # Install Nginx and configure with a custom HTTP header
-# Ensure that the package list is up to date
-exec { 'apt-update':
-    command     => '/usr/bin/env/apt-get -y update',
-    path        => '/usr/bin/env',
-    refreshonly => true,
-}
-
 # Install the Nginx package
 package { 'nginx':
     ensure  => installed,
-    require => Exec['apt-update'], # Ensure apt-update is executed first
 }
 
 # Create the index.html file with content "Hello World"
@@ -20,16 +12,13 @@ file { '/var/www/html/index.html':
 
 # Add the custom HTTP header response to Nginx configuration
 file_line { 'add-header':
-    ensure  => present,
-    path    => '/etc/nginx/sites-available/default',
-    line    => "\add_header X-Served-By ${hostname};",
-    after   => 'server_name _;',
-    require => Package['nginx'], # Ensure Nginx is installed before modifying the config
+    ensure => present,
+    path   => '/etc/nginx/sites-available/default',
+    line   => "\add_header X-Served-By ${hostname};",
+    after  => 'server_name _;',
 }
 
 # Ensure Nginx service is running
 service { 'nginx':
     ensure  => running,
-    enable  => true,
-    require => [File['/var/www/html/index.html'], File_line['add-header']], # Require necessary resources
 }
